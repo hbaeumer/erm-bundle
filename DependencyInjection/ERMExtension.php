cdbd6a7d5a0a5178a5aea373e5b9018cff0cd8dc
Heiner Bäumer <mail@hbaeumer.de>
2019-01-07 22:32:14 +0100

    inital commit

--- DependencyInjection/ERMExtension.php
+++ DependencyInjection/ERMExtension.php
@@ -30,13 +30,13 @@ namespace Hbaeumer\ErmBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\Loader;
 
 class ERMExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
     }
 
