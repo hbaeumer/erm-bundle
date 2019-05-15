867f4bb3fd1cb0ba7b4601c34c171a872c400040
Heiner Bäumer <mail@hbaeumer.de>
2019-05-16 00:13:26 +0200

    Release (#6)
    * #3 add Datacollector
    
    * #3 add Datacollector
    
    * #3 add Datacollector

---
+++ DataCollector/HbaeumerErmDataCollector.php
@@ -0,0 +1,54 @@
+<?php
+/*
+ * MIT License
+ *
+ * Copyright (c)  Heiner Baeumer
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+
+namespace Hbaeumer\ErmBundle\DataCollector;
+
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\DataCollector\DataCollector;
+
+class HbaeumerErmDataCollector extends DataCollector
+{
+    public function collect(Request $request, Response $response, \Exception $exception = null)
+    {
+        $this->data = [
+            'hbaeumer_erm' => [],
+        ];
+    }
+
+    public function getName()
+    {
+        return 'hbaeumer_erm';
+    }
+
+    public function reset()
+    {
+        $this->data = [];
+    }
+
+
+}
